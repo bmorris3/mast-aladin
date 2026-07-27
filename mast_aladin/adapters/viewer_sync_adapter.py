835b1b171f83b34ce38708e3f0c49833e1fc092f
Brett M. Morris <bmmorris@stsci.edu>
2026-07-27 14:09:40 -0400

    bugfix 100% courtesy of Celia's eagle eyes

--- mast_aladin/adapters/viewer_sync_adapter.py
+++ mast_aladin/adapters/viewer_sync_adapter.py
@@ -3,23 +3,28 @@ from mast_aladin.aida import AIDA_aspects
 
 
 class ViewerSyncAdapter(ABC):
-    def sync_to(self, sync_adapter, aspects):
+    def sync_to(self, sync_viewer, aspects):
+        source_viewer = sync_viewer.viewer
 
         # TODO (2026-07-22): the jdaviz glue viewer attribute `aid`
         # will be removed in a PR coming soon, the line below will
         # need to be updated.
-        viewer = sync_adapter.viewer
-        if hasattr(sync_adapter, 'aid'):
-            # temporarily, the jdaviz sync adapter has an `aid attribute`
-            viewer = sync_adapter.aid
+        if hasattr(source_viewer, '_obj'):
+            source_viewer = source_viewer._obj.glue_viewer.aid
 
-        source_viewport = viewer.get_viewport(sky_or_pixel="sky")
+        source_viewport = source_viewer.get_viewport(sky_or_pixel="sky")
 
-        new_viewport = viewer.get_viewport(sky_or_pixel="sky").copy()
+        new_viewport = source_viewer.get_viewport(sky_or_pixel="sky").copy()
         for aspect in set(aspects) & {*AIDA_aspects}:
             new_viewport[aspect] = source_viewport[aspect]
 
-        viewer.set_viewport(**new_viewport)
+        # TODO (2026-07-22): the jdaviz glue viewer attribute `aid`
+        # will be removed in a PR coming soon, the line below will
+        # need to be updated.
+        if hasattr(self.viewer, '_obj'):
+            self.viewer._obj.glue_viewer.aid.set_viewport(**new_viewport)
+        else:
+            self.viewer.set_viewport(**new_viewport)
 
     @abstractmethod
     def add_callback(self, func):
